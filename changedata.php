681ce839dc321efd7d880ce9482a673d0ff2a498
ovirchenko <38672954+ovirchenko@users.noreply.github.com>
2021-01-04 23:30:26 +0000

    Add files via upload

---
+++ changedata.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method=POST action="changedatahandler.php">
+        <input type="password" name="password" placeholder="Введите новый пароль">
+        <input type="password" name="password_confirm" placeholder="Повторите новый пароль">
+        <button>Сменить пароль</button>
+    </form>   
+</body>
+</html>
